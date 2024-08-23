1f4edefde45472815c77777ba7f4e0cb34624030
Cristian Olivares <cri.olivaresh@duocuc.cl>
2024-08-22 20:22:57 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projectgeo',
+  webDir: 'www'
+};
+
+export default config;
