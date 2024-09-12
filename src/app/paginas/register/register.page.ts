4e6153c7a64da846b17b684e20022ab68747d4f3
Cristian Olivares <cri.olivaresh@duocuc.cl>
2024-09-12 11:03:37 -0300

    añadido el registro en regiter

--- src/app/paginas/register/register.page.ts
+++ src/app/paginas/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+correo:string=""
+password:string=""
+usuario:string=""
+constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController) { }
+  
+async MensajeExito() {
+  const toast = await this.mensaje.create({
+    message: 'Cuenta creada con éxito, inicia sesión',
+    duration: 3000
+  });
+  toast.present();
+}
 
-  constructor() { }
-
-  ngOnInit() {
+async MensajeError() {
+  const alert = await this.alerta.create({
+    header: '',
+    subHeader: 'Fallo al crear tu cuenta',
+    message: 'Los datos ingresados no son válidos',
+    buttons:['Aceptar']
+  });
+  await alert.present();
+}
+ 
+registrar(){
+if (this.correo ==="" || this.password==="" || this.usuario==="") {
+  console.log("No pueden haber espacios en blanco")
+  this.MensajeError()
   }
+else {console.log("Creación de cuenta con éxito")
+  this.MensajeExito()
+  this.route.navigate(["/login"]) 
+
+} 
+}
+
+ngOnInit() {
+}
 
 }
