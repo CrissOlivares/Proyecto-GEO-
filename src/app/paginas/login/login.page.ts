9a751be9872e105c7affe390f583311846be5e5e
Cristian Olivares <cri.olivaresh@duocuc.cl>
2024-10-01 13:02:53 -0300

    agregados el localstore

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
+import { FormGroup, FormControl,
+  Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +10,22 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  correo:string=""
-  password:string=""
+formularioLogin: FormGroup;
 
-  constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController) { }
+  // // correo:string=""
+  // // password:string=""
+
+  constructor(public fb:FormBuilder, public mensaje:ToastController, private route:Router, public alerta:AlertController) {
+    this.formularioLogin = this.fb.group({
+      'usuario':new FormControl("",Validators.required),
+      'correo':new FormControl("",Validators.required),
+      'password':new FormControl("",Validators.required)
+    })
+   }
   
   async MensajeExito() {
     const toast = await this.mensaje.create({
-      message: 'Ingresando a GeoCash',
+      message: 'Ingresando...',
       duration: 1500
     });
     toast.present();
@@ -23,29 +33,46 @@ export class LoginPage implements OnInit {
 
   async MensajeError() {
     const alert = await this.alerta.create({
-      header: 'Error',
-      subHeader: 'Fallo al iniciar',
+      header: 'Error al iniciar',
+      subHeader: '',
       message: 'Las credenciales no son válidas, intentalo de nuevo',
       buttons:['Aceptar']
     });
     await alert.present();
 }
    
-ingresar(){
-  if (this.correo ==="" || this.password==="") {
-    console.log("No pueden haber espacios en blanco")
+// ingresa(){
+//   if (this.correo ==="" || this.password==="") {
+//     console.log("No pueden haber espacios en blanco")
     
-    }
-  if (this.correo ==="admin@admin" && this.password==="6818") {
-    console.log("Inicio exitoso")
-    this.MensajeExito()
-    this.route.navigate(["/home"])  
-  } else {this.MensajeError()
+//     }
+//   if (this.correo ==="admin@admin" && this.password==="6818") {
+//     console.log("Inicio exitoso")
+//     this.MensajeExito()
+//     this.route.navigate(["/home"])  
+//   } else {this.MensajeError()
   
-  } 
-  }
+//   } 
+//   }
 
   ngOnInit() {
   }
 
-}
+  async ingresar() {
+    var f = this.formularioLogin.value;
+    var usuarioString = localStorage.getItem('usuario');
+    if (usuarioString !== null) {
+      var usuario = JSON.parse(usuarioString);
+      if (usuario.correo == f.correo && usuario.password == f.password) {
+        localStorage.setItem('ingresado', 'true');
+        this.MensajeExito();
+      } else {
+        this.MensajeError();
+        };
+        
+      }
+    }
+
+  }
+ 
+
--- src/app/paginas/register/register.page.ts
+++ src/app/paginas/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController, ToastController } from '@ionic/angular';
+import { FormGroup, FormControl,
+  Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,10 +10,20 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+  formularioRegister: FormGroup;
+
 correo:string=""
 password:string=""
 usuario:string=""
-constructor(public mensaje:ToastController, private route:Router, public alerta:AlertController) { }
+constructor(public fb:FormBuilder,public mensaje:ToastController, private route:Router, public alerta:AlertController) {
+  this.formularioRegister = this.fb.group({
+    'usuario':new FormControl("",Validators.required),
+    'correo':new FormControl("",Validators.required),
+    'password':new FormControl("",Validators.required),
+    'confirmPassword':new FormControl("",Validators.required)
+ 
+  })
+ }
   
 async MensajeExito() {
   const toast = await this.mensaje.create({
@@ -23,14 +35,16 @@ async MensajeExito() {
 
 async MensajeError() {
   const alert = await this.alerta.create({
-    header: '',
-    subHeader: 'Fallo al crear tu cuenta',
-    message: 'Los datos ingresados no son válidos',
+    header: 'Fallo al crear tu cuenta',
+    subHeader: '',
+    message: 'Datos incompletos',
     buttons:['Aceptar']
   });
   await alert.present();
+  return;
 }
  
+// de momento esta sin uso
 registrar(){
 if (this.correo ==="" || this.password==="" || this.usuario==="") {
   console.log("No pueden haber espacios en blanco")
@@ -44,6 +58,24 @@ else {console.log("Creación de cuenta con éxito")
 }
 
 ngOnInit() {
+}
+
+async  registrarse(){
+  var f=this.formularioRegister.value;
+  if(this.formularioRegister.invalid){
+    this.MensajeError()
+  }
+  else{this.MensajeExito()}
+
+  var usuario ={
+    usuario:f.usuario,
+    correo:f.correo,
+    password:f.password
+  }
+
+  // aqui es donde se guardan los datos en local
+  localStorage.setItem('usuario', JSON.stringify(usuario))
+
 }
 
 }
