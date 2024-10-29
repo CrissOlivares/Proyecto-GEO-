ad36cbbafa13029d9359c43a9e5218acdcf6e6d7
Vina del Mar <vina@duoc.acad>
2024-10-29 11:53:56 -0300

    firebase2

--- src/app/servicios/firebase-login.service.ts
+++ src/app/servicios/firebase-login.service.ts
@@ -1,20 +1,41 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseLoginService {
 
-  constructor(private afAuth:AngularFireAuth, private router:Router) { }
+  constructor(public ngFireAuth:AngularFireAuth, private router:Router) { }
+  //creacion usuario
+async registerUser(email:string,password:string){
+  return await this.ngFireAuth.createUserWithEmailAndPassword(email,password);
+}
+   //login usuario
+async loginUser(email:string,password:string){
+  return await this.ngFireAuth.signInWithEmailAndPassword(email,password);
+}
+ //recuperacion usuario
+async resetPassword(email:string){
+  await this.ngFireAuth.sendPasswordResetEmail(email);
+}
+//logOut de usuario
+async logOut(){
+  return await this.ngFireAuth.signOut()
+}
+async getProfile(){
+  return await this.ngFireAuth.currentUser;
+}
 
-  login(email:string,password:string){
-    return this.afAuth.signInWithEmailAndPassword(email, password);
-  }
-  logout(){
-    return this.afAuth.signOut().then(()=>{
-      this.router.navigate(['/login']);
-    })
-  }
+
+  // login(email:string,password:string){
+  //   return this.ngFireAuth.signInWithEmailAndPassword(email, password);
+  // }
+  // logout(){
+  //   return this.ngFireAuth.signOut().then(()=>{
+  //     this.router.navigate(['/login']);
+  //   })
+  // }
 }
