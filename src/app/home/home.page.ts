1530678aa1204fa37853a566f9943e48a4927fb8
criss <cri.olivaresh@duocuc.cl>
2024-11-06 14:13:37 -0300

    maps en home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,12 @@
-import { Component, ViewChild} from '@angular/core';
+import { Component, viewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { IonFabList } from '@ionic/angular';
 import { FirebaseLoginService } from '../servicios/firebase-login.service';
+import { ViewChild,ElementRef, } from '@angular/core';
+
+
+declare var google:any;
+
 
 @Component({
   selector: 'app-home',
@@ -10,9 +15,44 @@ import { FirebaseLoginService } from '../servicios/firebase-login.service';
 })
 export class HomePage {
 user:any;
-  constructor(private route:Router,private router:Router,public authService:FirebaseLoginService) { 
-    
+map: any;
+
+@ViewChild('map',{read:ElementRef,static:false}) mapRef!: ElementRef;
+
+  constructor(private route:Router,private router:Router,
+    public authService:FirebaseLoginService
+  ) {}
+
+  ionViewDidEnter() {
+    this.showMap(); // Mostrar el mapa cuando la vista esté cargada
   }
+
+  showMap() {
+    // Verifica que 'google' y 'google.maps' estén disponibles antes de acceder a ellos
+    if (typeof google === 'undefined' || typeof google.maps === 'undefined') {
+      console.error('Google Maps no está cargado');
+      return;
+    }
+
+    // Definir la ubicación inicial del mapa
+    const location = new google.maps.LatLng(-17.824858, 31.053028);
+
+    // Opciones del mapa
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true, // Deshabilitar los controles predeterminados del mapa
+    };
+
+    // Crear el mapa
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options); // Usamos 'google.maps.Map'
+  }
+
+
+
+
+
+
 
   ngOnInit() {
     this.user = this.authService.getProfile();
