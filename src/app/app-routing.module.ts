fd6d9558cb862b7cb71fc2c440cc5664dfdb915d
criss <cri.olivaresh@duocuc.cl>
2024-11-06 22:49:32 -0300

    listo 404 y guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Error404Page } from './paginas/error404/error404.page';
+import { AuthGuard } from './guards/auth.guard';
+
+
+
 
 const routes: Routes = [
   
@@ -10,7 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),canActivate: [AuthGuard]
   },
   {
     path: 'login',
@@ -27,7 +32,8 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./paginas/register/register.module').then( m => m.RegisterPageModule)
-  },  {
+  },
+  {
     path: 'resetear-pass',
     loadChildren: () => import('./paginas/resetear-pass/resetear-pass.module').then( m => m.ResetearPassPageModule)
   },
@@ -35,6 +41,13 @@ const routes: Routes = [
     path: 'cuenta',
     loadChildren: () => import('./paginas/cuenta/cuenta.module').then( m => m.CuentaPageModule)
   },
+  {
+    path: 'error404',
+    loadChildren: () => import('./paginas/error404/error404.module').then( m => m.Error404PageModule)
+  },
+  // no encuentra pagina
+  { path: 'not-found', component: Error404Page },
+  { path: '**', redirectTo: 'not-found' }
 
 ];
 
