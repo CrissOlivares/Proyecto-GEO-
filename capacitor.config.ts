2525cd8b228e12a2396b178a6e749115e14063ad
Cristian Olivares <cri.olivaresh@duocuc.cl>
2024-12-03 19:49:40 -0300

    preparando proyecto

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'projectgeo',
+  appId: 'proyectGeo.beta',
+  appName: 'GeoCash',
   webDir: 'www'
 };
 
